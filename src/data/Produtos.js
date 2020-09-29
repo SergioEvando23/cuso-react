9ce1a69efbf0842a6dafba5e993164353bcdcb9a
SergioEvando23 <sergioevandocosta@gmail.com>
2020-09-28 22:02:39 -0300

    upgrade's card, produtos, components

--- src/data/Produtos.js
+++ src/data/Produtos.js
@@ -1,5 +1,5 @@
 export default [
-    { id: 1, nome: 'Caneta', preco: 7.29 },
+    { id: 1, nome: 'Caneta', preco: 7.28 },
     { id: 2, nome: 'Lapiz', preco: 1.29 },
     { id: 3, nome: 'Caderno', preco: 17.49 },
 
